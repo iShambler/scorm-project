d8ea759607bbe53abe5273e1a2eec51bda2ab81c
ARELANCE\pablo.solis <pablo.solis@arelance.com>
2026-02-25 14:00:51 +0100

    logo navegador

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SCORM Generator — ARELANCE</title>
+    <link rel="icon" type="image/png" href="assets/img/arelance-icono.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/styles.css">
